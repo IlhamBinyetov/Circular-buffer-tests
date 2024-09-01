2186c62bd556bbc4a48ee3edaeb8ffc0615d518d
Ilham Binyetov <ilhamgb@code.edu.az>
2024-09-01 18:36:36 +0400

    Add project files.

---
+++ CircularBuffer.cs
@@ -0,0 +1,73 @@
+namespace Circular_buffer
+{
+    public class CircularBuffer<T>
+    {
+        //////////////////////////////////////////////////////
+        // You can add your own methods but .:[DO NOT]:.
+        // change the signature of the provided methods
+        //////////////////////////////////////////////////////
+        ///
+        private T[] buffer;
+        private int head;
+        private int tail;
+        private bool isFull;
+
+        public CircularBuffer(int capacity)
+        {
+            if (capacity <= 0) throw new ArgumentException("Capacity must be greater than zero.");
+            buffer = new T[capacity];
+            head = 0;
+            tail = 0;
+            isFull = false;
+        }
+
+        public T Read()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Buffer is empty.");
+            var value = buffer[head];
+            head = (head + 1) % buffer.Length;
+            isFull = false;
+            return value;
+        }
+
+        public void Write(T value)
+        {
+            if (isFull)
+                throw new InvalidOperationException("Buffer is full.");
+
+            buffer[tail] = value;
+            tail = (tail + 1) % buffer.Length;
+
+            if (tail == head)
+                isFull = true;
+
+        }
+
+        public void Overwrite(T value)
+        {
+            if(isFull)
+            {
+                buffer[tail] = value;
+                tail = (tail + 1) % buffer.Length;
+                head = tail;
+            }
+        else
+            {
+                Write(value);
+            }
+        }
+
+        public void Clear()
+        {
+            head = 0;
+            tail = 0;
+            isFull = false;
+        }
+
+        private bool IsEmpty()
+        {
+            return !isFull && head == tail;
+        }
+    }
+}
---
+++ CircularBufferTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circular_buffer
+{
+    public class CircularBufferTests
+    {
+        /////////////////////////////////////////////////////////
+        // You can add more tests to validate your implementation
+        /////////////////////////////////////////////////////////
+
+        [Fact]
+        public void Reading_empty_buffer_should_fail()
+        {
+            var buffer = new CircularBuffer<int>(capacity: 1);
+            Assert.Throws<InvalidOperationException>(() => buffer.Read());
+        }
+
+        [Fact]
+        public void Can_read_an_item_just_written()
+        {
+            var buffer = new CircularBuffer<int>(capacity: 1);
+            buffer.Write(1);
+            Assert.Equal(1, buffer.Read());
+        }
+
+        [Fact]
+        public void Writing_to_full_buffer_should_fail()
+        {
+            var buffer = new CircularBuffer<int>(capacity: 1);
+            buffer.Write(1);
+            Assert.Throws<InvalidOperationException>(() => buffer.Write(2));
+        }
+
+        [Fact]
+        public void Overwriting_in_a_full_buffer_should_replace_oldest_item()
+        {
+            var buffer = new CircularBuffer<int>(capacity: 2);
+            buffer.Write(1);
+            buffer.Write(2);
+            buffer.Overwrite(3);
+            Assert.Equal(2, buffer.Read());
+            Assert.Equal(3, buffer.Read());
+        }
+
+        [Fact]
+        public void Clearing_buffer_should_make_it_empty()
+        {
+            var buffer = new CircularBuffer<int>(capacity: 2);
+            buffer.Write(1);
+            buffer.Clear();
+            Assert.Throws<InvalidOperationException>(() => buffer.Read());
+        }
+    }
+}
